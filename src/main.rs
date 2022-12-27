905ee499061f1013f6382438cc482d049615e605
Ari Johannesson <arividar@gmail.com>
2022-12-27 12:19:20 +0000

    first running version

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
 use win_screenshot::capture::*;
+use std::time::Duration;
+use std::thread;
+use ctrlc;
 
 fn main() {
-    let image = capture_display().unwrap();
-    image.save("screenshot.jpg").unwrap();
+    // set up Ctrl-C handling
+    ctrlc::set_handler(|| {
+        // this code will be executed when the user hits Ctrl-C
+        println!("Ctrl-C pressed!");
+
+        // exit the program
+        std::process::exit(0);
+    }).expect("Error setting Ctrl-C handler");
+
+    let mut i = 0;
+    loop {
+        i = i + 1;
+        // create a new thread to run the job
+        let handle = thread::spawn(move || {
+            let mut filename = String::from("screenshot");
+            filename.push_str(&i.to_string());
+            filename.push_str(".jpg");
+            println!("**** Writing filename: {}", filename);
+            let image = capture_display().unwrap();
+            image.save(filename).unwrap();
+            println!("Running job...{}", i);
+        });
+
+        // sleep for 10 seconds
+        thread::sleep(Duration::from_secs(10));
+
+        // wait for the job to finish
+        handle.join().unwrap();
+    }
 }
